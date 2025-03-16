6e02dbba791b3597a044e08a22c2d937b5410f86
Adil Bukhari <adilbukhari@Adils-MacBook-Pro.local>
2025-03-16 00:22:21 -0700

    Debloat

--- losses/snr.py
+++ losses/snr.py
@@ -14,34 +14,23 @@ See the License for the specific language governing permissions and
 limitations under the License.
 '''
 
-import os
 import torch as th
-import numpy as np
-import sys
-sys.path.append('/home/tianqiwei/jupyter/rob_IR/')
-import datasets
-import configs
-from utility import utils
-import torch
-import torch.nn as nn
+import torch.nn.functional as F
 from .miner import miner
 import functools as ft
 import itertools as it
 import pytest
-import torch.nn.functional as F
-import rich
-c = rich.get_console()
-
 
 def fn_psnr_kernel(repA: th.Tensor, repP: th.Tensor, repN: th.Tensor, *,
-                   margin: float = configs.snr.margin,
-                   reg_lambda: float = configs.snr.reg_lambda):
+                   margin: float,
+                   reg_lambda: float):
     '''
     Raw functional version of SPC-2 SNR.
     https://github.com/Confusezius/Revisiting_Deep_Metric_Learning_PyTorch
     '''
-    pos_snr = th.var(repA - repP, dim=1) / th.var(repA, dim=1)
-    neg_snr = th.var(repA - repN, dim=1) / th.var(repA, dim=1)
+    var_A = th.var(repA, dim=1)
+    pos_snr = th.var(repA - repP, dim=1) / var_A
+    neg_snr = th.var(repA - repN, dim=1) / var_A
     reg_los = th.mean(th.abs(th.sum(repA, dim=1)))
     snr_los = (pos_snr - neg_snr + margin).relu()
     snr_los = th.sum(snr_los) / th.sum(snr_los > 0)
@@ -50,7 +39,7 @@ def fn_psnr_kernel(repA: th.Tensor, repP: th.Tensor, repN: th.Tensor, *,
 
 
 def fn_psnr(repres: th.Tensor, labels: th.Tensor,
-            *, metric: str, minermethod: str, p_switch: float = -1.0):
+            *, metric: str, minermethod: str, p_switch: float = -1.0, margin: float, reg_lambda: float):
     '''
     SNR Loss function for DML
     '''
@@ -59,9 +48,9 @@ def fn_psnr(repres: th.Tensor, labels: th.Tensor,
         repres = F.normalize(repres, p=2, dim=-1)
     # Sample the triplets
     anc, pos, neg = miner(repres, labels, method=minermethod,
-                          metric=metric, margin=configs.snr.margin, p_switch=p_switch)
+                          metric=metric, margin=margin, p_switch=p_switch)
     # Calculate Loss
-    loss = fn_psnr_kernel(repres[anc, :], repres[pos, :], repres[neg, :])
+    loss = fn_psnr_kernel(repres[anc, :], repres[pos, :], repres[neg, :], margin=margin, reg_lambda=reg_lambda)
     return loss
 
 
@@ -69,13 +58,16 @@ class psnr(th.nn.Module):
     _datasetspec = 'SPC-2'
     _minermethod = 'spc2-random'
 
-    def __call__(self, *args, **kwargs):
-        if hasattr(self, '_minermethod'):
-            return ft.partial(fn_psnr, metric=self._metric,
-                              minermethod=self._minermethod)(*args, **kwargs)
-        else:
-            return ft.partial(fn_psnr, metric=self._metric)(
-                *args, **kwargs)
+    def __init__(self, metric: str, margin: float, reg_lambda: float):
+        super().__init__()
+        self._metric = metric
+        self._margin = margin
+        self._reg_lambda = reg_lambda
+
+    def forward(self, repres: th.Tensor, labels: th.Tensor, p_switch: float = -1.0):
+        return fn_psnr(repres, labels, metric=self._metric,
+                       minermethod=self._minermethod, p_switch=p_switch,
+                       margin=self._margin, reg_lambda=self._reg_lambda)
 
     def determine_metric(self):
         return self._metric
@@ -85,31 +77,34 @@ class psnr(th.nn.Module):
 
     def raw(self, repA, repP, repN):
         print('snr raw is called!')
-        return fn_psnr_kernel(repA, repP, repN)
+        return fn_psnr_kernel(repA, repP, repN, margin=self._margin, reg_lambda=self._reg_lambda)
 
 
 class psnrC(psnr):
-    _metric = 'C'
+    def __init__(self, margin: float, reg_lambda: float):
+        super().__init__(metric='C', margin=margin, reg_lambda=reg_lambda)
 
 
 class psnrE(psnr):
-    _metric = 'E'
+    def __init__(self, margin: float, reg_lambda: float):
+        super().__init__(metric='E', margin=margin, reg_lambda=reg_lambda)
 
 
 class psnrN(psnr):
-    _metric = 'N'
+    def __init__(self, margin: float, reg_lambda: float):
+        super().__init__(metric='N', margin=margin, reg_lambda=reg_lambda)
 
 
 @pytest.mark.parametrize('metric, minermethod', it.product('NEC',
                                                            ('spc2-random', 'spc2-distance', 'spc2-hard', 'spc2-softhard', 'spc2-semihard')))
 def test_fn_psnr(metric: str, minermethod: str):
     output, labels = th.rand(10, 32, requires_grad=True), th.randint(3, (10,))
-    loss = fn_psnr(output, labels, metric=metric, minermethod=minermethod)
+    loss = fn_psnr(output, labels, metric=metric, minermethod=minermethod, margin=0.1, reg_lambda=0.001)
     loss.backward()
 
 
 @pytest.mark.parametrize('func', [psnrC, psnrE, psnrN])
 def test_psnr(func: object):
     output, labels = th.rand(10, 32, requires_grad=True), th.randint(3, (10,))
-    loss = func()(output, labels)
+    loss = func(margin=0.1, reg_lambda=0.001)(output, labels)
     loss.backward()
